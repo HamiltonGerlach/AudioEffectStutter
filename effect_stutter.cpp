89144c658051ad366e294cb59baded9376b9c145
Hamilton Gerlach <aboehmvie@gmail.com>
2022-07-15 20:33:55 +0200

    Initial commit of effect_stutter / AudioEffectStutter

---
+++ effect_stutter.cpp
@@ -0,0 +1,86 @@
+#include <Arduino.h>
+#include "effect_stutter.h"
+
+void AudioEffectStutter::update(void)
+{
+#if defined(__ARM_ARCH_7EM__)
+	audio_block_t *blocka;
+    int i;
+    
+	blocka = receiveReadOnly(0);
+	if (!blocka) { return; }
+    
+    switch (state)
+    {
+        case 0:            
+            transmit(blocka);
+            release(blocka);
+            
+            break;
+            
+        case 1:
+            if (queue[position]) { release(queue[position]); }
+            
+            queue[position] = blocka;
+            transmit(blocka);
+            
+            position = (position < (STUTTER_QUEUE_SIZE - 1)) ? position + 1 : 0;
+            length = (length < (STUTTER_QUEUE_SIZE - 1)) ? length + 1 : length;
+            
+            break;
+            
+        case 2:
+            release(blocka);
+            
+            i = (head + offset) % (STUTTER_QUEUE_SIZE - 1);
+            
+            if (queue[i]) { transmit(queue[i]); }
+            
+            head = (head < (length - 1)) ? head + 1 : 0;
+            
+            break;
+    }
+#elif defined(KINETISL)
+	audio_block_t *block;
+
+	block = receiveReadOnly(0);
+	if (block) release(block);
+#endif
+}
+
+
+void AudioEffectStutter::snap() {
+    if (state == 1) { return; }
+    
+    offset = position;
+    state = 1;
+}
+
+void AudioEffectStutter::latch() {
+    if (!state) { return; }
+    
+    head = 0;
+    state = 2;
+}
+
+void AudioEffectStutter::unlatch() {
+    if (!state) { return; }
+    
+    offset = 0;
+    head = 0;
+    position = 0;
+    length = 0;
+    state = 0;
+}
+
+bool AudioEffectStutter::isActive() {
+    return (state > 0) ? true : false;
+}
+
+bool AudioEffectStutter::isSnapped() {
+    return (state == 1) ? true : false;
+}
+
+bool AudioEffectStutter::isLatched() {
+    return (state == 2) ? true : false;
+}
---
+++ effect_stutter.h
@@ -0,0 +1,43 @@
+#ifndef effect_stutter_h_
+#define effect_stutter_h_
+#include "Arduino.h"
+#include "AudioStream.h"
+#include "utility/dspinst.h"
+
+
+#if defined(__IMXRT1062__)
+  // 4.00 second maximum on Teensy 4.0
+  #define STUTTER_QUEUE_SIZE  14000 // (176512 / AUDIO_BLOCK_SAMPLES)
+#elif defined(__MK66FX1M0__)
+  // 2.41 second maximum on Teensy 3.6
+  #define DELAY_QUEUE_SIZE  (106496 / AUDIO_BLOCK_SAMPLES)
+#elif defined(__MK64FX512__)
+  // 1.67 second maximum on Teensy 3.5
+  #define DELAY_QUEUE_SIZE  (73728 / AUDIO_BLOCK_SAMPLES)
+#elif defined(__MK20DX256__)
+  // 0.45 second maximum on Teensy 3.1 & 3.2
+  #define DELAY_QUEUE_SIZE  (19826 / AUDIO_BLOCK_SAMPLES)
+#else
+  // 0.14 second maximum on Teensy 3.0
+  #define DELAY_QUEUE_SIZE  (6144 / AUDIO_BLOCK_SAMPLES)
+#endif
+
+class AudioEffectStutter : public AudioStream
+{
+public:
+	AudioEffectStutter() : AudioStream(1, inputQueueArray) { }
+	virtual void update(void);
+    void snap(); // snap (set loop start)
+    void latch(); // latch (loop from start to current position)
+    void unlatch(); // release and return to passthrough mode
+    bool isActive(); // is snapped (listening) or latched (looping)
+    bool isSnapped(); // is snapped (listening)
+    bool isLatched(); // is latched (looping)
+private:
+	audio_block_t *inputQueueArray[1];
+	audio_block_t *queue[STUTTER_QUEUE_SIZE];
+    uint16_t position = 0, offset = 0, head = 0, length = 0; // 0 ... STUTTER_QUEUE_SIZE
+    int state = 0; // 0 ... passthrough, 1 ... snap, 2 ... latch
+};
+
+#endif
